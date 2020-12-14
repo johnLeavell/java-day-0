63ec502ef5f794477127da733b368af08b37f389
John Leavell <johnleavell@gmail.com>
2020-12-14 16:48:31 -0600

    "implementing email constructor"

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -1,5 +1,17 @@
 package emailapp;
 
 public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
 	
+	//Constructor to receive firstName and lastName
+	// Ask for the dept
+	// Generate a random password
+	// set mailbox capacity
+	// set alternate email
+	// change the password
 }
