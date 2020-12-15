cafbe4c6f5cf5dff766eb2bea35ce34a048b558a
John Leavell <johnleavell@gmail.com>
2020-12-14 18:56:57 -0600

    Update Email.java

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -9,16 +9,13 @@ public class Email {
 	private String alternateEmail;
 	
 	//Constructor to receive firstName and lastName
-<<<<<<< HEAD
+
 	public Email(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		System.out.println("Email Created:" + this.firstName + " " + this.lastName);
 		
 	}
-	
-=======
->>>>>>> 63ec502ef5f794477127da733b368af08b37f389
 	// Ask for the dept
 	// Generate a random password
 	// set mailbox capacity
